0064519117720e9d8533ff02fb3667bd142932fc
JulietteBLC <juliette.blanc@devinci.fr>
2017-04-25 18:43:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ xmlhttp.onreadystatechange = function() {
 };
 xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
 xmlhttp.send();
+
+function homePageLoading() {
+    url = 'apiUrl';
+    elementID = 'idDeVotreElementHtml';
+    fonctionRequeteApi(url, elementID);
+
+    ...
+}
+
