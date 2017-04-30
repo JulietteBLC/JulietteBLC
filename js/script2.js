f36a564e17167e344ef464597ca2abd3357cf34c
JulietteBLC <juliette.blanc@devinci.fr>
2017-04-30 20:08:03 +0200

    Create script2.js

---
+++ js/script2.js
@@ -0,0 +1,17 @@
+<div class=interieur>
+		
+				<h1>Générateur de QRCode</h1>
+				<p> Voici un QRCode : </p>
+					<div>
+						<div id="qrcode"></div>
+						<script type="text/javascript">
+						var qrcode = new QRCode(document.getElementById("qrcode"), {
+							text: "http://base.io",
+							width: 128,
+							height: 128,
+							colorDark : "#000000 »,
+							colorLight : "#ffffff",
+							correctLevel : QRCode.CorrectLevel.H
+						});
+					</div>
+		</div>
